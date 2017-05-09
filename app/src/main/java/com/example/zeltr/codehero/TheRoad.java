a95f780549acde5eed11b5ed151cdb4aace9a386
etharion <warlordcp@gmail.com>
2017-05-09 14:51:23 +0200

    the road, ev handlers

--- app/src/main/java/com/example/zeltr/codehero/TheRoad.java
+++ app/src/main/java/com/example/zeltr/codehero/TheRoad.java
@@ -2,6 +2,7 @@ package com.example.zeltr.codehero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TheRoad extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class TheRoad extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_road);
+    }
+
+    protected void startQuest1(View view){
+        //Intent intent = new Intent(this, TheRoad1Activity.class);
+        //startActivity(intent);
+    }
+
+    protected void startQuest2(View view){
+        //Intent intent = new Intent(this, TheRoad2Activity.class);
+        //startActivity(intent);
+    }
+
+    protected void startQuest3(View view){
+        //Intent intent = new Intent(this, TheRoad3Activity.class);
+        //startActivity(intent);
+    }
+
+    protected void startQuest4(View view){
+        //Intent intent = new Intent(this, TheRoad4Activity.class);
+        //startActivity(intent);
     }
 }
