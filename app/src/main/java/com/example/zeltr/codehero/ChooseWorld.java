1d11bcf4eb94aa2724164106768a9f6879ddedb7
Zeltres <Zeltres@hotmail.com>
2017-05-09 17:01:48 +0200

    Slide animation added and US2 done beside User Test

--- app/src/main/java/com/example/zeltr/codehero/ChooseWorld.java
+++ app/src/main/java/com/example/zeltr/codehero/ChooseWorld.java
@@ -1,13 +1,37 @@
 package com.example.zeltr.codehero;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class ChooseWorld extends AppCompatActivity {
+public class ChooseWorld extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_world);
+    }
+
+    protected void startTheRoad(View view){
+        Intent intent = new Intent(this, TheRoad.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_in_left);
+    }
+
+    protected void startTheForest(View view){
+        //Intent intent = new Intent(this, TheForest.class);
+        //startActivity(intent);
+    }
+
+    protected void startTheCastle(View view){
+        //Intent intent = new Intent(this, TheCastle.class);
+        //startActivity(intent);
+    }
+
+    protected void showCharacter(View view){
+        //Intent intent = new Intent(this, Character.class);
+        //startActivity(intent);
     }
 }
