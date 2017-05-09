0a776d8b21cf76dfcaf41e43193559380c7c62e5
Mcairborne <Mcairborne1000@gmail.com>
2017-05-09 13:12:45 +0200

    Eventhandling og UI for story 1

--- app/src/main/java/com/example/zeltr/codehero/MainActivity.java
+++ app/src/main/java/com/example/zeltr/codehero/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.zeltr.codehero;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void startQuest(View view){
+        //Intent intent = new Intent(this, LevelselectActivity.class);
+        //startActivity(intent);
+    }
+    protected void showCharacter(View view){
+        //Intent intent = new Intent(this, CharacterActivity.class);
+        //startActivity(intent);
+    }
+    protected void socialize(View view){
+        //Intent intent = new Intent(this, SocializeActivity.class);
+        //startActivity(intent);
+    }
+    protected void showAchievements(View view){
+        //Intent intent = new Intent(this, AchievementsActivity.class);
+        //startActivity(intent);
     }
 }
