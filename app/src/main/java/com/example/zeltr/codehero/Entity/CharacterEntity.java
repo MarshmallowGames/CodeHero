48ca307100247011b25b493e7ba59f019896e07c
tsvetelintsonev <github.tsonev@yahoo.com>
2017-05-12 11:56:29 +0200

    CharacterEntity.java updated

--- app/src/main/java/com/example/zeltr/codehero/Entity/CharacterEntity.java
+++ app/src/main/java/com/example/zeltr/codehero/Entity/CharacterEntity.java
@@ -7,6 +7,11 @@ public class CharacterEntity {
     private String gender;
     private List<ItemEntity> items;
 
+    public String getGender() { return gender; }
 
+    public void setGender(String gender) { this.gender = gender; }
 
+    public List<ItemEntity> getItems() { return items; }
+
+    public void setItems(List<ItemEntity> items) { this.items = items; }
 }
