e8cf6fd9cf0bea3193d09cdc749da008721393dc
Zeltres <Zeltres@hotmail.com>
2017-06-01 08:29:27 +0200

    Few changes

--- app/src/main/java/com/example/zeltr/codehero/Persistence/DBInitializer.java
+++ app/src/main/java/com/example/zeltr/codehero/Persistence/DBInitializer.java
@@ -8,7 +8,7 @@ public class DBInitializer extends SQLiteOpenHelper {
 
     public DBInitializer(Context context) {
         super(context, DBInfo.DATABASE_NAME, null, DBInfo.DATABASE_VERSION);
-        context.deleteDatabase(DBInfo.DATABASE_NAME);
+        //context.deleteDatabase(DBInfo.DATABASE_NAME);
         this.getWritableDatabase();
     }
 
